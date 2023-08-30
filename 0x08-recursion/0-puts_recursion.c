1b36bf4dcc52a4dc77284c62ad16bd2b6de9a1e3
Era_Psy20 <Era-P20sy@outlook.fr>
2023-08-30 12:52:28 -0700

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Main - Prints a string followed by a new line using recursion.
+ * @s: The input string.
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')  
+    {
+        _putchar('\n');  
+        return;
+    }
+    
+}
+
